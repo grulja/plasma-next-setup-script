8c64f462df469bbff376da204ddbe4d0b76a9906
Jan Grulich <jgrulich@redhat.com>
2014-07-16 13:41:07 +0200

    Update dependecies, packages, paths

--- kde5.sh
+++ kde5.sh
@@ -1,9 +1,9 @@
-export XDG_DATA_DIRS="/usr/share/kf5:/usr/share/kde5:/usr/share"
-export QT_PLUGIN_PATH="/usr/lib64/kde5/plugins:/usr/lib64/qt5/plugins/kf5:/usr/lib64/qt5/plugins:/usr/lib64/plugins:/usr/lib64/plugins/kf5"
-export QML2_IMPORT_PATH="/usr/lib64/qt5/qml:/usr/lib64/kde5/qml:/usr/lib64/qml"
-export LD_LIBRARY_PATH="/usr/lib64/kde5:/usr/lib64"
-export XDG_ICON_DIRS="/usr/share/icons"
-export LIBEXEC_PATH="/usr/libexec/kde5:/usr/libexec/kf5:/usr/libexec"
+export XDG_DATA_DIRS=/usr/share/kf5:/usr/share/kde5:/usr/share:$XDG_DATA_DIRS
+export QT_PLUGIN_PATH=/usr/lib64/kde5/plugins:/usr/lib64/qt5/plugins/kf5:/usr/lib64/qt5/plugins:/usr/lib64/plugins:/usr/lib64/plugins/kf5:$QT_PLUGIN_PATH
+export QML2_IMPORT_PATH=/usr/lib64/qt5/qml:/usr/lib64/kde5/qml:/usr/lib64/qml:$QML2_IMPORT_PATH
+export LD_LIBRARY_PATH=/usr/lib64/kde5:/usr/lib64:$LD_LIBRARY_PATH
+export XDG_ICON_DIRS=/usr/share/icons:$XDG_ICON_DIRS
+export LIBEXEC_PATH=/usr/libexec/kde5:/usr/libexec/kf5:/usr/libexec:$LIBEXEC_PATH
 export KDE_SESSION_VERSION=5
 export KDE_FULL_SESSION=true
 export QT_QUICK_CONTROLS_STYLE=Breeze
